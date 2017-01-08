399834fc7d3852da3b580662537454440020e12b
Topher <hughestopher86@gmail.com>
2017-01-07 18:02:00 -0800

    added stuff

---
+++ Assets/Scripts/Elemental.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elemental : MonoBehaviour {
+
+	public float damage = 1.0f;
+	public float range = 10.0f;
+	public float cooldown = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private Enemy getClosestEnemy(){
+		foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Enemy")){
+			float dist = (obj.transform.position - transform.position).sqrMagnitude;
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float health = 10.0f;
+	public float speed = 1.0f;
+	public float damage = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+}
